7a43ab19f2e0466ce4735da45da97b9a4e77a6b3
Admin-PC\Admin <pil717171@gmail.com>
2019-01-23 11:28:45 +0300

    add routing

--- src/js/main.js
+++ src/js/main.js
@@ -1,88 +1,54 @@
+var body = document.querySelector('body')
+var coord = 0
+var about = document.querySelector('.about')
+var contact = document.querySelector('.contact')
+var photo = document.querySelector('.photo')
 
-(function(){
-    window.onscroll = CoordinateTiresHeader; 
-    function CoordinateTiresHeader () {
-        document.querySelectorAll(".tires-images").forEach(function (images) {
-            var coordImg = images.getBoundingClientRect()
-                if (coordImg.y < document.documentElement.clientHeight) {
-                    drive2(images);
-                    document.querySelectorAll(".tires-images_tire_wheel").forEach(function (wheel) {
-                        var coord2 = wheel.getBoundingClientRect()
-                        if (coord2.y < document.documentElement.clientHeight) {
-                            drive(wheel);
-                        }
-                    })    
-                }
-            function drive (element) {
-                element.classList.add("tires-images_tire_wheel--rotate")   
-                };
-                
-            function drive2 (element) {
-                element.classList.add("tires-images--drive")
-                };       
-            })
-            
-        document.querySelectorAll(".tires_revers-images").forEach(function (imagesRevers) {
-            var coordImgRevers = imagesRevers.getBoundingClientRect()
-                if (coordImgRevers.y < document.documentElement.clientHeight) {
-                    drive4(imagesRevers);
-                    document.querySelectorAll(".tires_revers-images_tire_wheel").forEach(function (wheelRevers) {
-                        var coord3 = wheelRevers.getBoundingClientRect()
-                        if (coord3.y < document.documentElement.clientHeight) {
-                            drive3(wheelRevers);
-                        }
-                    })    
-                }    
-      
-            function drive3 (elementReverse) {
-            elementReverse.classList.add("tires_revers-images_tire_wheel--rotate")   
-            };
-            
-            function drive4 (elementReverse) {
-            elementReverse.classList.add("tires_revers-images--drive")
-            };
-        })
-        console.log("dsknsnk")
-        var headerTop = document.querySelector(".header-top")
-        var header = document.querySelector(".header")
-        var coord = headerTop.getBoundingClientRect()
-        var height = coord.height
-        var scrolling = window.pageYOffset
+about.addEventListener('click', () => {
+    coord = -100
+    body.style.transform = "translateY("+coord+"%)"
+    return coord
+})
+contact.addEventListener('click', () => {
+    coord = -200
+    body.style.transform = "translateY("+coord+"%)"
+    return coord
+})
+photo.addEventListener('click', () => {
+    coord = -300
+    body.style.transform = "translateY("+coord+"%)"
+    return coord
+})
 
-        if (scrolling > height) {
-            header.classList.add("header--none")
+window.addEventListener('wheel', (e) => {
+    if (e.deltaY < 0) {
+        if (coord >= 0) {
+            body.style.transform = "translateY(0%)"
         }
         else {
-            header.classList.remove("header--none")
+            coord +=100
+            body.style.transform = "translateY("+coord+"%)"
         }
-    }       
-})();
-    
+        
+    }
+    else if (e.deltaY > 0) {
+        if (coord <= -300) {
+            body.style.transform = "translateY(-300%)"
+        }
+        else {
+            coord -=100
+            body.style.transform = "translateY("+coord+"%)"
+        }
+    }
+})
 
-    // (function(){
+// var c = 0
+// window.addEventListener('wheel', (e) => {
+//     if (e.deltaY < coord) {
+//         body.style.transform = "translateY(100%)" 
+//     }
+//     else {
+//         body.style.transform = "translateY(-100%)"
+//     }
+// })
 
-    //     document.querySelectorAll(".tires").forEach(function (tires){
-    //         tires.onmouseover = function coordinate () {
-    //             document.querySelectorAll(".tires_revers-images").forEach(function (images) {
-    //                 var coordImg = images.getBoundingClientRect()
-    //                 if (coordImg.y < document.documentElement.clientHeight) {
-    //                     drive2(images);
-    //                     document.querySelectorAll(".tires_revers-images_tire_wheel").forEach(function (wheel) {
-    //                         var coord2 = wheel.getBoundingClientRect()
-    //                         if (coord2.y < document.documentElement.clientHeight) {
-    //                             drive(wheel);
-    //                         }
-    //                     })    
-    //                 }
-    //             })
-    //         };
-          
-    //         function drive (element) {
-    //             element.classList.add("tires_revers-images_tire_wheel--rotate")   
-    //         };
-            
-    //         function drive2 (element) {
-    //             element.classList.add("tires_revers-images--drive")
-    //         };
-    //     })        
-    // })();
